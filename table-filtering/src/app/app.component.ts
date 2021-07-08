60fe7729fbc9ac0386e4ac5a33fd4cbc2233dacb
dreamnajababy <patnarin_s@kkumail.com>
2021-07-08 18:24:35 +0700

    filtering datatable

--- table-filtering/src/app/app.component.ts
+++ table-filtering/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatTableDataSource} from '@angular/material/table';
+
+
+export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'table-filtering';
+    title = 'table-filtering';
+    displayedColumns: string[] = ['name'];
+    dataSource = new MatTableDataSource([]);
+
+    constructor(private http:HttpClient){
+    }
+
+    async ngOnInit(){
+        const URL = `https://api.publicapis.org/categories`
+            this.http.get(URL).subscribe( (data:any) => {
+            this.dataSource = new MatTableDataSource(data.map( (item:string) => {return {name:item}}))
+        })
+    }
+    public emitFilter(e:any){
+        let value = e.target.value
+        this.dataSource.filter = value.trim().toLowerCase();
+    }
 }
--- table-filtering/src/app/app.module.ts
+++ table-filtering/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
