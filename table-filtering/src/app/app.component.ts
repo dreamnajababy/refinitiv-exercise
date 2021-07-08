cc407da36e71551dc7a215a53716afded0237f09
dreamnajababy <patnarin_s@kkumail.com>
2021-07-08 19:06:40 +0700

    refactor filtering with grid system

--- table-filtering/src/app/app.component.ts
+++ table-filtering/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+
 
 
 export interface PeriodicElement {
@@ -15,20 +17,25 @@ export interface PeriodicElement {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     title = 'table-filtering';
     displayedColumns: string[] = ['name'];
     dataSource = new MatTableDataSource([]);
 
-    constructor(private http:HttpClient){
-    }
+    @ViewChild(MatPaginator) paginator:any;
 
-    async ngOnInit(){
+    ngOnInit(){
         const URL = `https://api.publicapis.org/categories`
             this.http.get(URL).subscribe( (data:any) => {
             this.dataSource = new MatTableDataSource(data.map( (item:string) => {return {name:item}}))
+            this.dataSource.paginator = this.paginator;
         })
     }
+    ngAfterViewInit() {
+    }
+    constructor(private http:HttpClient){
+    }
+
     public emitFilter(e:any){
         let value = e.target.value
         this.dataSource.filter = value.trim().toLowerCase();
--- table-filtering/src/app/app.module.ts
+++ table-filtering/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import {HttpClientModule} from '@angular/common/http';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         MatTableModule,
         MatFormFieldModule,
         MatInputModule,
+        MatGridListModule,
+        MatPaginatorModule,
         BrowserAnimationsModule,
         HttpClientModule
     ],
